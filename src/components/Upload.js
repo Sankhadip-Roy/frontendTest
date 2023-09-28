68b36fbc80c5c25ded898cca3de36e00d41b3c6f
SayanGhosh10 <Sayanmyself50@gmail.com>
2023-09-28 11:38:09 +0530

    Added android and ios

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -14,9 +14,20 @@ function Upload(props) {
   const passAlert = () => {
     props.showAlert("Your Picture is being analyzed", "success");
 }
+const mediaQueries = {
+  desktop: '@media (min-width: 1024px)',
+  android : '@media (max-width: 768px)',
+};
 let myStyle = {
-  width: "100vh",
+  width: "50%",
   position: "center",
+  maxWidth: "500px",
+}
+if (window.matchMedia(mediaQueries.desktop).matches) {
+  myStyle.maxWidth = '800px';
+}
+if (window.matchMedia(mediaQueries.android).matches) {
+  myStyle.maxWidth = '250px';
 }
 let stl = {
   color : 'white',
@@ -29,14 +40,31 @@ let stl = {
                     <input className="form-control" type="text" placeholder="" aria-label="default input example"/>
             </div>
             <div className="mb-3">
-              <label htmlFor="exampleFormControlTextarea1" className="form-label" style={stl}>Describe your problem</label>
-              <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label htmlFor="exampleFormControlTextarea1" className="form-label" style={stl}>Enter Location</label>
+            <div className="col-md-6">
+                <label htmlFor="inputState" className="form-label" style={stl}>State</label>
+                <select id="inputState" className="form-select">
+                <option value>Choose...</option>
+                <option>West Bengal</option>
+                <option>Uttar Pradesh</option>
+                <option>Bihar</option>
+                </select>
+            </div>
+            <div className="col-md-6">
+                <label htmlFor="inputCity" className="form-label" style={stl}>City</label>
+                <select id="inputCity" className="form-select">
+                <option value>Choose...</option>
+                <option>Howrah</option>
+                <option>Kolkata</option>
+                <option>Hoogly</option>
+                </select>
+            </div>
             </div>
             <div>
               <p style={stl}>Upload Image</p>
               <input className="form-control" type="file" onChange={handleChange} />
               <div className="text-center">
-              <img  style={myStyle} className='my-5' src={file} />
+              <img  style={myStyle} className='image my-5' src={file} />
               </div>
             </div>
             <button type="submit" onClick={passAlert} className="my-3 btn btn-success text-center">Analyze</button>
